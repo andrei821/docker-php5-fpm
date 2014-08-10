dfa8b3d8199f9e32cbdfe5f7fd98325231531671
andrei821 <andrei@mondoit.ro>
2014-08-10 19:35:55 +0300

    Update Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@
 if [ -z "$LISTEN" ]; then
         echo "Please add your listen mode (socker / ip)"
    else
-        sed 's|listen = /var/run/php5-fpm.sock|listen = '$FPM'|' -i /etc/php5/fpm/pool.d/www.conf
+        sed 's|listen = /var/run/php5-fpm.sock|listen = '$LISTEN'|' -i /etc/php5/fpm/pool.d/www.conf
 fi
 /usr/sbin/php5-fpm
